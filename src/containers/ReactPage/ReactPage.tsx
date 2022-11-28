60cea35cc342a315140f7f7e874e4547c7047b49
Pavlov Vladimir <connection.lost.again@gmail.com>
2022-11-28 11:25:41 +0600

    Created ReactPage component

--- src/containers/ReactPage/ReactPage.tsx
+++ src/containers/ReactPage/ReactPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import reactLogo from "../../assets/react-logo.svg";
-import tsLogo from "../../assets/ts-logo.svg";
 
 const ReactPage = () => {
   return (
@@ -10,19 +9,50 @@ const ReactPage = () => {
           <div>
             <div className="text-center">
               <img className="Home-logo" src={reactLogo} alt="React-logo"/>
-              <img className="Home-logo" src={tsLogo} alt="ts-logo"/>
             </div>
             <h1 className=" my-4 text-center fs-3">
-              <span className="fw-bold d-block my-2">React &amp; Typescript</span>
-              <span className="fw-light d-block fs-5">Build powerful and robust apps using modern technologies.</span>
+              <span className="fw-bold d-block my-2">React</span>
+              <span className="fw-light d-block fs-5">A JavaScript library for building user interfaces.</span>
             </h1>
           </div>
         </div>
       </section>
       <section>
         <div className="container">
-          <article className="custom-mw py-5">
-            some text
+          <article className=" py-5">
+            <div className="row">
+              <div className="col">
+                <h2 className="fs-4">Declarative</h2>
+                <p>
+                  React makes it painless to create interactive UIs. Design simple views for each state
+                  in your application, and React will efficiently update and render
+                  just the right components when your data changes
+                </p>
+                <p>
+                  Declarative views make your code more predictable and easier to debug.
+                </p>
+              </div>
+              <div className="col">
+                <h2 className="fs-4">Component-Based</h2>
+                <p>
+                  Build encapsulated components that manage their own state, then compose them to make complex UIs.
+                </p>
+                <p>
+                  Since component logic is written in JavaScript instead of templates, you can
+                  easily pass rich data through your app and keep state out of the DOM.
+                </p>
+              </div>
+              <div className="col">
+                <h2 className="fs-4">Learn Once, Write Anywhere</h2>
+                <p>
+                  We don’t make assumptions about the rest of your technology stack, so you can develop
+                  new features in React without rewriting existing code.
+                </p>
+                <p>
+                  React can also render on the server using Node and power mobile apps using React Native.
+                </p>
+              </div>
+            </div>
           </article>
         </div>
       </section>
